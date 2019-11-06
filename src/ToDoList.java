dc67318e571011452fba23991d563706d236d31d
Andrew <andrew.low1@ncs.com.sg>
2019-11-06 10:12:20 +0800

    No comment

---
+++ src/ToDoList.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+public class ToDoList {
+	private HashMap<String, Task> tasks = new HashMap<String, Task>();
+
+	public void addTask(Task task) {
+		// Add code here
+		if (task != null) {
+			tasks.put(task.getDescription(), task);
+		}
+	}
+
+	public void completeTask(String description) {
+		// Add code here
+		Task task = tasks.get(description);
+		task.setComplete(true);
+		tasks.put(task.getDescription(), task);
+
+	}
+
+	public boolean getStatus(String description) {
+		// Add code here
+		return tasks.get(description).isComplete();
+	}
+
+	public Task getTask(String description) {
+		// Add code here
+		return tasks.get(description);
+	}
+
+	public Task removeTask(String description) {
+		// Add code here
+		Task task = tasks.get(description);
+		tasks.remove(description);
+		return task;
+	}
+
+	public Collection<Task> getAllTasks() {
+		return tasks.values();
+	}
+
+	public Collection<Task> getCompletedTasks() {
+		// Add code here
+		Collection<Task> result = new HashSet<Task>();
+		for (Task task : getAllTasks()) {
+			if (task.isComplete()) {
+				result.add(task);
+			}
+		}
+		return result;
+	}
+}
---
+++ test/ToDoListTest.java
@@ -0,0 +1,75 @@
+import java.util.Collection;
+
+import org.junit.*;
+import org.junit.Test;
+import junit.framework.*;
+import java.util.Set;
+import java.util.HashSet;
+
+public class ToDoListTest extends TestCase{
+	// Define Test Fixtures
+	private ToDoList toDoList;
+	private Task task1;
+	private Task task2;
+
+	
+	public ToDoListTest() {
+		super();
+		toDoList = new ToDoList();
+	}
+	@Before
+	 public void setUp() throws Exception{
+		//Initialise Test Fixtures
+		task1 = new Task("task1", false);
+		task2 = new Task("task2", false);
+	}
+	@After
+	 public void tearDown() throws Exception{
+		// Uninitialise test Fixtures
+		toDoList = new ToDoList();
+	}
+
+	@Test
+	public void testAddTask() {
+		assertTrue(toDoList.getAllTasks().isEmpty());
+		toDoList.addTask(task1);
+		assertFalse(toDoList.getAllTasks().isEmpty());
+		assertEquals(toDoList.getTask(task1.getDescription()), task1);
+	}
+	@Test
+	public void testGetStatus() {
+		toDoList.addTask(task1);
+		boolean status = toDoList.getStatus(task1.getDescription());
+		assertEquals(status, task1.isComplete());
+		toDoList.completeTask(task1.getDescription());
+		assertEquals(true, toDoList.getStatus(task1.getDescription()));
+	}
+	@Test
+	public void testRemoveTask() {
+		toDoList.addTask(task1);
+		assertNotNull(toDoList.getTask(task1.getDescription()));
+		toDoList.removeTask(task1.getDescription());
+		assertNull(toDoList.getTask(task1.getDescription()));
+	}
+	@Test
+	public void testGetCompletedTasks() {
+		toDoList.addTask(task1);
+		toDoList.addTask(task2);
+		Set<Task> result = (Set<Task>) toDoList.getCompletedTasks();
+		assertTrue(result.isEmpty());
+		for (Task task : result) {
+			if (!task.isComplete()) {
+				fail();
+			}
+		}
+		task1.setComplete(true);
+		toDoList.addTask(task1);
+		result = (Set<Task>) toDoList.getCompletedTasks();
+		assertFalse(result.isEmpty());
+		for (Task task : result) {
+			if (!task.isComplete()) {
+				fail();
+			}
+		}
+	}
+}
